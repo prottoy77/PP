284b85855d49f4a20bd62f2d708b79b7b46bddcb
Nazirul Islam77 <135655739+prottoy77@users.noreply.github.com>
2023-07-11 19:11:59 +0600

    Update main.php

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
     include('header.php');
     // Your main content goes 
     
-    echo "Hello my name is hira";
+    echo "Hello my name is nazirul";
     // Add the content for the different pages of your website
     include('footer.php');
 ?>
