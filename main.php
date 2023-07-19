f871f64470d86d4504c987f89abadbfb2cc3b292
Nazirul Islam77 <135655739+prottoy77@users.noreply.github.com>
2023-07-19 15:14:59 +0600

    Delete main.php

--- main.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include('header.php');
-    // Your main content goes 
-    
-    echo "Hello my name is nazirul";
-    // Add the content for the different pages of your website
-    include('footer.php');
-?>
